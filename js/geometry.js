0a6ac36e291303febbf78dd771bb67c7c7a3b4b2
BFIbrahim <ibrahim74543@gmail.com>
2023-02-18 23:26:16 -0800

    three card completed

--- js/geometry.js
+++ js/geometry.js
@@ -1,11 +1,55 @@
+// Card one Completed
 document.getElementById('tringle-calc-btn').addEventListener('click', function () {
-    const tringleNameElement = document.getElementById('tringle-name');
-    const tringleName = tringleNameElement.innerText;
+    // const tringleNameElement = document.getElementById('tringle-name');
+    // const tringleName = tringleNameElement.innerText;
+    const tringleName = getElementName('tringle-name')
 
     const tringleBaseValue = getInputValue('tringle-Base-field')
     const tringleHeightValue = getInputValue('tringle-height-field')
 
     const tringleResult = 0.5 * tringleBaseValue * tringleHeightValue;
+
+    const tringleBase = document.getElementById('tringle-base-value');
+    const tringleHeight = document.getElementById('tringle-height-value')
+    tringleBase.innerText = tringleBaseValue
+    tringleHeight.innerText = tringleHeightValue;
+
     
     setResult(tringleName, tringleResult)
+})
+
+// card Two Completed
+document.getElementById('rectangle-btn-calc').addEventListener('click', function(){
+    const rectangleName = getElementName('rectangle-Name');
+
+    const rectangleWidthValue = getInputValue('rectangle-W-field');
+    const rectangleHeightValue = getInputValue('rectangle-l-field');
+
+    const rectangleResult = rectangleWidthValue * rectangleHeightValue;
+
+    const rectangleWidth = document.getElementById('rectangle-w-value')
+    const rectangleHidth = document.getElementById('rectangle-h-value')
+    rectangleWidth.innerText = rectangleWidthValue;
+    rectangleHidth.innerText = rectangleHeightValue
+
+    setResult(rectangleName, rectangleResult)
+
+})
+
+// Card Three Completed
+document.getElementById('parallelogram-btn-calc').addEventListener('click', function(){
+    const parallelogramName = getElementName('parallelogram-name');
+    
+    const parallelogramBaseValue = getInputValue('parallelogram-base-field')
+    const parallelogramHightValue = getInputValue('parallelogram-height-field')
+
+    const parallelogramResult = parallelogramBaseValue * parallelogramHightValue;
+
+    const parallelogramBase = document.getElementById('parallelogram-b-value')
+    const parallelogramHeight = document.getElementById('parallelogram-h-value')
+    parallelogramBase.innerText = parallelogramBaseValue;
+    parallelogramHeight.innerText = parallelogramHightValue;
+
+    setResult(parallelogramName, parallelogramResult)
+
 })
--- js/utilitis.js
+++ js/utilitis.js
@@ -1,3 +1,10 @@
+// get the Element name
+function getElementName(elementId){
+    const elementname = document.getElementById(elementId);
+    const elementnameInnerText = elementname.innerText;
+    return elementnameInnerText
+}
+
 // Get the Input Value
 function getInputValue(elementId){
     const userInputElement = document.getElementById(elementId);
@@ -12,7 +19,7 @@ function getInputValue(elementId){
 function setResult(resultItemName, finalResult){
     const resultFieldContainer = document.getElementById('result-field-container');
 
-    let btn = document.createElement("button");
+    const btn = document.createElement("button")
     btn.innerHTML = 'Covert to m' + '2'.sup();
 
     const tr = document.createElement('tr');
@@ -22,5 +29,6 @@ function setResult(resultItemName, finalResult){
         <td>${finalResult}</td>
         <td>${btn.innerHTML}</td>
     `
+
     resultFieldContainer.appendChild(tr)
 }
